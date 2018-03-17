b982fc58136b0c378cd5684cc198b30cbbc93b9d
dragon8github <928532756@qq.com>
2018-03-17 20:02:40 +0800

    123

--- src/views/CarSell/CarSellHistory.vue
+++ src/views/CarSell/CarSellHistory.vue
@@ -2,8 +2,8 @@
     <div class="CarSellHistory">
 
         <navbar v-model="selected">
-            <tabitem id="tab-container0" :fontSize="'32'">已竞买</tabitem>
-            <tabitem id="tab-container1" :fontSize="'32'">已报名</tabitem> 
+            <tabitem id="tab-container1" :fontSize="'32'">已竞买</tabitem>
+            <tabitem id="tab-container2" :fontSize="'32'">已报名</tabitem> 
         </navbar>
 
         <div class="search">
@@ -11,37 +11,37 @@
                 <i class="search__icon"></i>
                 <input type="text" class="search__input" placeholder="请输入品牌/型号" v-model="search" />
             </div>
-            <div class="search__right"> 搜索 </div>
+            <div class="search__right" @click="searchFn"> 搜索 </div>
         </div>
 
         <tabcontainer  v-model="selected" swipeable>
-            <tabcontaineritem  id="tab-container0" >
-              <panel ref="pannel0" :_loadTop = "loadTop" :_loadBottom = "loadBottom" :_isEmpty="tag[0]['isEmpty']" :_bottomDisabled = "tag[0]['bottomDisabled']">
+            <tabcontaineritem  id="tab-container1" >
+              <panel ref="pannel1" :_loadTop = "loadTop" :_loadBottom = "loadBottom" :_isEmpty="tag['1']['isEmpty']" :_bottomDisabled = "tag['1']['bottomDisabled']">
                   <div slot="body">
-                     <item v-for="(item,index) in tag[0]['list']" 
-                           :key="index"
-                           :image="item.image"
-                           :name="item.name"
-                           :money="item.money"
-                           :time="item.time"
-                           :city="item.city"
-                           :id="item.id">
+                     <item v-for="(item,index) in tag['1']['list']" :key="index"                           
+                           :id="item.businessId"
+                           :maindata="item"
+                           :image="item.docs && item.docs[0] && item.docs[0].docUrl"
+                           :name="item.vehicleBrand"
+                           :money="item.startPrice"
+                           :endttime="item.etartPriceDate"
+                           :city="item.vcehicleTerritory">
                      </item>
                   </div>
               </panel>
             </tabcontaineritem>
 
-            <tabcontaineritem id="tab-container1">
-              <panel ref="pannel1" :_loadTop = "loadTop" :_loadBottom = "loadBottom"  :_isEmpty="tag[1]['isEmpty']" :_bottomDisabled = "tag[1]['bottomDisabled']">
+            <tabcontaineritem id="tab-container2">
+              <panel ref="pannel2" :_loadTop = "loadTop" :_loadBottom = "loadBottom"  :_isEmpty="tag['2']['isEmpty']" :_bottomDisabled = "tag['2']['bottomDisabled']">
                   <div slot="body">
-                      <item v-for="(item,index) in tag[1]['list']" 
-                            :key="index"
-                            :image="item.image"
-                            :name="item.name"
-                            :money="item.money"
-                            :time="item.time"
-                            :city="item.city"
-                            :id="item.id">
+                      <item v-for="(item,index) in tag['2']['list']" :key="index"                            
+                            :id="item.businessId"
+                            :maindata="item"
+                            :image="item.docs && item.docs[0] && item.docs[0].docUrl"
+                            :name="item.vehicleBrand"
+                            :money="item.startPrice"
+                            :endttime="item.etartPriceDate"
+                            :city="item.vcehicleTerritory">
                       </item>
                   </div>
               </panel>
@@ -64,33 +64,33 @@
  import tabcontainer from '@components/tabcontainer/tabcontainer.vue'
  import tabcontaineritem from '@components/tabContainerItem/tabContainerItem.vue'
 
+ // Toast组件
+ import Toast from '@components/toast/index.js'
+
+ // 路由
+ import Router from 'vue-router'
+
+/**
+ * 注意：type其实就是tag
+ * 这是一一对应的。只是换一种理解和思维而已。
+ */
+
 export default {
   name: 'CarSellHistory',
-
   data () {
     return {
-        selected: 'tab-container0',
+        phone: this.$store.state.phone,
+        selected: 'tab-container1',
         search: '',
         where: {
-              tag: 0,          // 标签页[0：已竞买，1：已报名]
-              type: '',        // 型号
-              business_id: '', // 拍卖编号
-              page_index: 1,   // 分页索引，从1开始
-              page_size: 15    // 每次获取的条数
+            type: '1',                           // 1.已报名，2已竞买
+            telephone: this.$store.state.phone,  // 手机号码
+            page: 1,                             // 分页索引，从1开始
+            limit: 15,                           // 每次获取的条数
         },
         tag: {
-            '0':{ isSearch:false, isEmpty: false, isLoading: false, LoadingTimer: 0, list:[
-               { image: '', name: '奔驰B级车 A 200 时尚型奔驰B级车 A 200 时尚型', money: '15,000', time: '2018/03/20', city: '东莞市', id: 'HTPM201702281234' },
-              { image: '', name: '奔驰A级车 A 200 时尚型奔驰B级车 A 200 时尚型', money: '15,000', time: '2018/03/20', city: '东莞市', id: 'HTPM201702281234' },
-              { image: '', name: '奔驰A级车 A 200 时尚型奔驰B级车 A 200 时尚型', money: '15,000', time: '2018/03/20', city: '东莞市', id: 'HTPM201702281234' },
-              { image: '', name: '奔驰A级车 A 200 时尚型奔驰B级车 A 200 时尚型', money: '15,000', time: '2018/03/20', city: '东莞市', id: 'HTPM201702281234' },
-              { image: '', name: '奔驰A级车 A 200 时尚型奔驰B级车 A 200 时尚型', money: '15,000', time: '2018/03/20', city: '东莞市', id: 'HTPM201702281234' }, ], bottomDisabled: true, index: 0},
-            '1':{ isSearch:false, isEmpty:false,isLoading: false, LoadingTimer: 0, list:[
-                 { image: '', name: '奔驰B级车 A 200 时尚型奔驰B级车 A 200 时尚型', money: '15,000', time: '2018/03/20', city: '东莞市', id: 'HTPM201702281234' },
-                { image: '', name: '奔驰A级车 A 200 时尚型奔驰B级车 A 200 时尚型', money: '15,000', time: '2018/03/20', city: '东莞市', id: 'HTPM201702281234' },
-                { image: '', name: '奔驰A级车 A 200 时尚型奔驰B级车 A 200 时尚型', money: '15,000', time: '2018/03/20', city: '东莞市', id: 'HTPM201702281234' },
-                { image: '', name: '奔驰A级车 A 200 时尚型奔驰B级车 A 200 时尚型', money: '15,000', time: '2018/03/20', city: '东莞市', id: 'HTPM201702281234' },
-                { image: '', name: '奔驰A级车 A 200 时尚型奔驰B级车 A 200 时尚型', money: '15,000', time: '2018/03/20', city: '东莞市', id: 'HTPM201702281234' }], bottomDisabled: false, index: 1}
+            '1':{ isSearch:false, isEmpty: false, bottomDisabled: false, list:[] },
+            '2':{ isSearch:false, isEmpty: false, bottomDisabled: false, list:[] },
         },
         oldTag: null
     }
@@ -106,43 +106,79 @@ export default {
   computed: {
       // 返回当前的tag对象
       currTag () {
-        return this.tag[this.where.tag]
+        return this.tag[this.where.type]
       }
   },
   methods: {
-    loadTop (cb) {
-        window.setTimeout(cb, 1000);
+    getData (success_cb, err_cb, isQuite = false) {
+        this.carapi.selectAuctionReg(this.where, isQuite).then(data => {
+           if (data.returnCode == 0) {
+              // 当请求数据不为空的时候，重置展示状态
+              if (data.data.length > 0) {
+                this.currTag.bottomDisabled = false;
+                this.currTag.isEmpty = false;
+              } else {
+                this.currTag.isEmpty = true;
+              }
+              success_cb && success_cb(data)
+           } else {
+              err_cb && err_cb(data.msg ? data : { msg: '网络异常' })
+           }
+       })
     },
-    getData () {
-        // this.xdapi.getRepayingList({
-        //       pageIndex: '1',  // 页数
-        //       pageSize: '10'   // 数量
-        // }).then(data=>{
-        //     if (data.returnCode == 0) {
-        //         console.log(data);
-        //     } else {
-        //         console.log(data);
-        //         Toast(data.msg);
-        //     }
-        // })
+    loadTop (cb) {
+        // 据我所知，下拉刷新需要重置一下搜索条件。
+        this.resetWhere()
+        this.getData(_ => {
+            this.currTag.list = _.data
+            cb && cb()
+        }, _=>{
+            Toast(_.msg)
+            cb && cb()
+        }, true)
     },
     loadBottom (cb) {
-         this.currTag.list.push(
-             { image: '', name: '奔驰B级车 B 200 时尚型奔驰B级车 B 200 时尚型', money: '15,000', time: '2018/03/20', city: '东莞市', id: 'HTPM201702281234' },
-             { image: '', name: '奔驰B级车 B 200 时尚型奔驰B级车 B 200 时尚型', money: '15,000', time: '2018/03/20', city: '东莞市', id: 'HTPM201702281234' },
-             { image: '', name: '奔驰B级车 B 200 时尚型奔驰B级车 B 200 时尚型', money: '15,000', time: '2018/03/20', city: '东莞市', id: 'HTPM201702281234' },
-             { image: '', name: '奔驰B级车 B 200 时尚型奔驰B级车 B 200 时尚型', money: '15,000', time: '2018/03/20', city: '东莞市', id: 'HTPM201702281234' },
-             { image: '', name: '奔驰B级车 B 200 时尚型奔驰B级车 B 200 时尚型', money: '15,000', time: '2018/03/20', city: '东莞市', id: 'HTPM201702281234' });
-         cb && cb();   
+        // 兼容一种特殊情况,只在多个tag且用户操作过急的情况才可能发生
+        if (this.$refs[`pannel${this.where.type}`].loading === false) {
+          this.$refs[`pannel${this.oldTag}`].$el.scrollTop = 0
+          return cb && cb()
+        }
+        // 页面索引++
+        this.where.page++;
+        this.getData(_ => {
+            // 如果请求数据为空，那就禁止【上拉加载更多】，展示【没有更多数据啦~】
+            if (_.data.length === 0) tag.bottomDisabled = true;
+            this.currTag.list.push(..._.data)
+            cb && cb()
+        }, _ => {
+            // 这个还不太好弄，如果说你处于下拉状态，只是关闭还没有用。因为还是会不断的触发下拉。这是一个bug。需要改正。
+            Toast(_.msg)
+            cb && cb()
+        }, true)
+    },
+    searchFn () {
+        this.resetWhere();
+        this.where.vehicleBrand = this.search
+        this.where.carModel = this.search
+
+        this.currTag.list = []
+        this.getData(_ => {
+            this.currTag.list = _.data
+            if (!this.currTag.list.length) {
+              this.currTag.isEmpty = true
+            }
+        }, _ => {
+            Toast(_.msg);
+            this.currTag.isEmpty = true
+        })
     },
     resetWhere () {
         this.where = {
-              tag: 0,          // 标签页[0：已竞买，1：已报名]
-              type: '',        // 型号
-              business_id: '', // 拍卖编号
-              page_index: 1,   // 分页索引，从1开始
-              page_size: 15    // 每次获取的条数
-        };
+            type: '1',                           // 1.已报名，2已竞买
+            telephone: this.$store.state.phone,  // 手机号码
+            page: 1,                             // 分页索引，从1开始
+            limit: 15,                           // 每次获取的条数
+        }
         this.search = '';
     }
   },
@@ -154,11 +190,25 @@ export default {
          // 重置搜索条件
          this.resetWhere();
          // 【设置当前操作tag对象】
-         this.where.tag =  curVal.substr(-1, 1)
+         this.where.type = curVal.substr(-1, 1)
+
+         // 选项卡改变的时候，肯定需要进行fetch的，但如果原本的tag有值的话，那么就不需要更新了。先这样简单处理。
+         // 这个页面，最好也加入active，就算更新出了问题，那也是以后的解决方案。
+         if (this.currTag.list.length == 0) {
+             this.getData(_ => {
+                this.currTag.list = _.data
+             }, _ => {
+                Toast(_.msg)
+             })
+         }
     }
   },
   beforeMount () {
-
+      this.getData(_ => {
+          this.currTag.list = _.data
+       }, _ => {
+          Toast(_.msg)
+       })
   },
   activated () {
 
@@ -177,7 +227,7 @@ export default {
 .search {
     @include flex(b, center);
     padding: 0 pxToRem(30px);
-    margin-top: pxToRem(10px);
+    margin: pxToRem(10px) 0;
     background: #fff;
     font-size: pxToRem(32px);
     height: pxToRem(98px);
